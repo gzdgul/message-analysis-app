492b5c82b16788a6d19f1b3b9f4ca7864eb3476e
gzdgul <gzdgul00@gmail.com>
2023-08-05 20:06:58 +0300

    graphic animation added

---
+++ components/AnalysisBar.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {View as MotiView} from "moti/build/components/view";
+import {LinearGradient} from "expo-linear-gradient";
+import {COLORS} from "../config/constants";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import { Dimensions } from 'react-native';
+
+const AnalysisBar = ({ id, percentageOfChange, pressedDay, setPressedDay, day, elementCount}) => {
+    const { width, height } = Dimensions.get('window');
+    const areaWidth =  Math.floor(width)-35
+    const barWidth = pressedDay === id ? areaWidth/elementCount - 5 + 15 : areaWidth/elementCount - 5
+    console.log(areaWidth)
+    const handleBarPress = () => {
+        setPressedDay(id)
+    }
+    return (
+        <TouchableOpacity onPress={handleBarPress} style={{flexDirection: 'column', }}>
+            <MotiView
+                transition={{ delay: 10, damping: 20, mass: 0.8 }}
+                animate={{
+                    height: pressedDay === id ? 160 - percentageOfChange : 150 - percentageOfChange,
+                    width: barWidth
+                }}
+                exit={{
+                    opacity: 0,
+                    width: 0,
+                    height: 0
+                }}
+                exitTransition={{
+                    type: 'timing',
+                    duration: 300,
+                }}
+                style={[styles.bar]}
+            >
+                {
+                    percentageOfChange < 75 &&
+                    <LinearGradient
+                        style={{ width: '100%', height: '100%', borderRadius: 15, position: 'absolute'}}
+                        colors={[COLORS.red, COLORS.orange, COLORS.orange]}
+                        start={{ x: 0, y: 0 }}
+                        end={{ x: 0, y: 1 }}
+                        pointerEvents="none"
+                    />
+                }
+            </MotiView>
+            <MotiView
+                transition={{ delay: 10, damping: 20, mass: 0.8 }}
+                animate={{
+                    width: barWidth
+                    // scale: (areaWidth/elementCount - 5)*0.15 > 3 ? 3 : (areaWidth/elementCount - 5)*0.15
+                }}
+                exit={{
+                    opacity: 0,
+                    width: 0,
+                }}
+                exitTransition={{
+                    type: 'timing',
+                    duration: 300,
+                }}
+                style={{height: 33, alignItems: 'center',}}
+            >
+                <View style={{flexDirection: 'row', paddingVertical: 10,  justifyContent: 'center' }} >
+                    <Text  numberOfLines={1} style={{color: 'white', fontSize: 4 + (barWidth)*0.1,  transform: [{ rotate: '270deg' }] }}>{day}</Text>
+                </View>
+            </MotiView>
+        </TouchableOpacity>
+    );
+};
+
+export default AnalysisBar;
+
+const styles = StyleSheet.create({
+
+    textStyle: {
+        color: COLORS.white,
+        fontSize: 17
+    },
+    bar: {
+        backgroundColor: COLORS.orange,
+        borderRadius: 15,
+
+    },
+});
--- components/advancedMessageAnalysis.js
+++ components/advancedMessageAnalysis.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View} from "react-native";
+import {Pressable, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {COLORS} from "../config/constants";
 import {addMissingDates, calculateCountAverage, findMaxCount, sumCounts} from "./utils";
 import {LinearGradient} from "expo-linear-gradient";
+import {View as MotiView} from "moti/build/components/view";
+import AnalysisBar from "./AnalysisBar";
+import {AnimatePresence} from "moti";
 
 const AdvancedMessageAnalysis = ({analyzedData}) => {
-
+    const [page, setPage] = React.useState(0)
+    const [pressedDay, setPressedDay] = React.useState('')
     const mostRepeatedDates = analyzedData.mostRepeatedDates
     const names = analyzedData.allSendings.nameCount
     const firstDate = mostRepeatedDates[0].date
@@ -19,107 +23,135 @@ const AdvancedMessageAnalysis = ({analyzedData}) => {
     // console.log('mostRepeatedDates!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',mostRepeatedDates)
     // console.log('mostRepeatedDatesSorted!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',mostRepeatedDatesSorted)
     // console.log('mostRepeatedDateeee!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',mostRepeatedDateeee)
-    const aaa =  test.slice(0,50)
-    const deneme = aaa.slice(0,7)
-    const deneme2 = aaa.slice(7,14)
+    const aaa =  test.slice(0,100)
+
+    // console.log('aaa!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',aaa)
+    // Verileri tarihlerine göre gruplamak için boş bir nesne oluşturuyoruz
+    let gruplanmisVeriler = {};
+
+// Veriler dizisini tarihlerine göre gruplayan bir döngü
+    aaa.forEach(function(veri) {
+        const tarihParcalari = veri.date.split('.');
+        const ay = '**.' + tarihParcalari[1] + '.' + tarihParcalari[2];
+
+        if (!gruplanmisVeriler[ay]) {
+            gruplanmisVeriler[ay] = [];
+        }
+
+        gruplanmisVeriler[ay].push(veri);
+    });
+    console.log('gruplanmisVeriler!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',gruplanmisVeriler)
+    const deneme = []
+    for (const ay in gruplanmisVeriler) {
+        deneme.push(gruplanmisVeriler[ay])
+    }
+    console.log('deneme//////////////',deneme)
     const baslangicTarih = aaa[0].date
     const bitisTarih = aaa[aaa.length -1].date
-    console.log('aaa!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',aaa)
+
+    const handlePressBack = () => {
+        if (deneme[deneme.length -1 - (page + 1)]) {
+            setPage(prevState => prevState + 1)
+        }
+    }
+    const handlePressNext = () => {
+        if (deneme[deneme.length -1 - (page - 1)]) {
+            setPage(prevState => prevState - 1)
+        }
+    }
+
 
     return (
         <View>
             <Text style={styles.mainTitle}>Message Analysis Advanced</Text>
-            <Text style={styles.dateTitle}>{baslangicTarih} - {bitisTarih}</Text>
-            <ScrollView horizontal={true}>
-                <View style={{}}>
-                <View style={{flexDirection: 'row', gap: 5, alignItems: 'flex-end'}}>
-                    {
-                        aaa.map((x, index) => {
-                            const percentageOfChange = ((maxMessageCount - x.count)/maxMessageCount)*150
-                            const day = x.date.split('.')[0]
-                            return (
-                            <View key={x.date} style={{flexDirection: 'column'}}>
-                                <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                                    <Text style={{color: 'white', fontSize: 12}}>{day}</Text>
-                                </View>
-                                <View  style={[styles.bar, {height: 150 - percentageOfChange}]}>
+            <Text style={styles.mainTitle}>{page}</Text>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <TouchableOpacity style={{padding: 10, backgroundColor: 'red'}} onPress={ handlePressBack }>
+                    <Text>GERİ</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{padding: 10, backgroundColor: 'yellow'}} onPress={ handlePressNext }>
+                    <Text>İLERİ</Text>
+                </TouchableOpacity>
+            </View>
 
+            <Text style={styles.dateTitle}>{deneme[deneme.length -1 - page][0].date} - {deneme[deneme.length -1 - page][deneme[deneme.length -1 - page].length - 1].date}</Text>
+                        <View>
+                            <View style={{paddingHorizontal: 10}}>
+                                <View style={{flexDirection: 'row', gap: 5,alignItems: 'flex-end',height: 183}}>
+                                    <AnimatePresence>
                                     {
-                                        percentageOfChange < 50 &&
-                                        <LinearGradient
-                                            style={{ width: '100%', height: '100%', borderRadius: 15, position: 'absolute'}}
-                                            colors={[COLORS.red, COLORS.orange, COLORS.orange]}
-                                            start={{ x: 0, y: 0 }}
-                                            end={{ x: 0, y: 1 }}
-                                            pointerEvents="none"
-                                        />
+                                        deneme[deneme.length -1 - page].map((x, index) => {
+                                            const percentageOfChange = ((maxMessageCount - x.count)/maxMessageCount)*150
+                                            const day = x.date.split('.')[0]
+                                            const elementCount =  deneme[deneme.length -1 - page].length
+                                            return (
+                                                   <AnalysisBar key={index} id={x.date} percentageOfChange={percentageOfChange} pressedDay={pressedDay} setPressedDay={setPressedDay} day={day} elementCount={elementCount} />
+                                            )
+                                        })
                                     }
+                                    </AnimatePresence>
                                 </View>
+                                {/*<View style={{flexDirection: 'row', gap: 5, alignItems: 'flex-start', opacity: 0.5, marginTop: 5}}>*/}
+                                {/*    {*/}
+                                {/*        deneme[deneme.length -1 - page].map((x) => {*/}
+                                {/*            const totalEmoji = [].concat(... x[names[0]].emoji).length + [].concat(... x[names[1]].emoji).length*/}
+                                {/*            const totalMedya = [].concat(... x[names[0]].media).length + [].concat(... x[names[1]].media).length*/}
+                                {/*            const totalOthers = [].concat(... x[names[0]].others).length + [].concat(... x[names[1]].others).length*/}
+                                {/*            const percentageOfChange = ((maxMessageCount - x.count)/maxMessageCount)*150*/}
+                                {/*            const percentageOfTotalEmoji = Math.ceil((totalEmoji / (totalEmoji + totalMedya + totalOthers))*100)*/}
+                                {/*            const percentageOfTotalMedya =  Math.ceil((totalMedya / (totalEmoji + totalMedya + totalOthers))*100)*/}
+                                {/*            const percentageOfTotalOthers =  Math.ceil((totalOthers / (totalEmoji + totalMedya + totalOthers))*100)*/}
+
+                                {/*            const day = x.date.split('.')[0]*/}
+                                {/*            return (*/}
+                                {/*                <View key={x.date} style={{flexDirection: 'column', gap: 5, height: 50,flex: 1,}}>*/}
+                                {/*                    {*/}
+                                {/*                        percentageOfTotalMedya > 0 &&*/}
+                                {/*                        <View style={[styles.bar, {backgroundColor: COLORS.green}, {flex: percentageOfTotalMedya}]}>*/}
+                                {/*                        </View>*/}
+                                {/*                    }*/}
+                                {/*                    {*/}
+                                {/*                        percentageOfTotalEmoji > 0 &&*/}
+                                {/*                        <View style={[styles.bar,{backgroundColor: COLORS.pink}, {flex: percentageOfTotalEmoji}]}>*/}
+                                {/*                        </View>*/}
+                                {/*                    }*/}
+                                {/*                    {*/}
+                                {/*                        percentageOfTotalOthers > 0 &&*/}
+                                {/*                        <View style={[styles.bar, {backgroundColor: COLORS.blue},{flex: percentageOfTotalOthers}]}>*/}
+                                {/*                        </View>*/}
+                                {/*                    }*/}
+
+                                {/*                    /!*<Text style={{color: 'white'}}>{percentageOfTotalEmoji}</Text>*!/*/}
+                                {/*                </View>*/}
+                                {/*            )*/}
+                                {/*        })*/}
+                                {/*    }*/}
+                                {/*</View>*/}
                             </View>
-                            )
-                        })
-                    }
-                </View>
-                <View style={{flexDirection: 'row', gap: 5, alignItems: 'flex-start', opacity: 0.5, marginTop: 5}}>
-                    {
-                        aaa.map((x) => {
-                            const totalEmoji = [].concat(... x[names[0]].emoji).length + [].concat(... x[names[1]].emoji).length
-                            const totalMedya = [].concat(... x[names[0]].media).length + [].concat(... x[names[1]].media).length
-                            const totalOthers = [].concat(... x[names[0]].others).length + [].concat(... x[names[1]].others).length
-                            const percentageOfChange = ((maxMessageCount - x.count)/maxMessageCount)*150
-                            const percentageOfTotalEmoji = Math.ceil((totalEmoji / (totalEmoji + totalMedya + totalOthers))*100)
-                            const percentageOfTotalMedya =  Math.ceil((totalMedya / (totalEmoji + totalMedya + totalOthers))*100)
-                            const percentageOfTotalOthers =  Math.ceil((totalOthers / (totalEmoji + totalMedya + totalOthers))*100)
+                            <View style={{width: '100%', height: 80, position: 'absolute', justifyContent: 'flex-end', pointerEvents: 'none'}}>
+                                <Text style={{color: 'white', opacity: 0.5, alignSelf: 'flex-end'}}>average</Text>
+                                <Text numberOfLines={1} ellipsizeMode="clip" style={{color: 'white', opacity: 0.5}}
+                                >_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ </Text>
 
-                            const day = x.date.split('.')[0]
-                            return (
-                                <View key={x.date} style={{flexDirection: 'column', gap: 5, height: 80}}>
-                                    {
-                                        percentageOfTotalMedya > 0 &&
-                                        <View style={[styles.bar, {backgroundColor: COLORS.green}, {flex: percentageOfTotalMedya}]}>
-                                        </View>
-                                    }
-                                    {
-                                        percentageOfTotalEmoji > 0 &&
-                                        <View style={[styles.bar,{backgroundColor: COLORS.pink}, {flex: percentageOfTotalEmoji}]}>
-                                        </View>
-                                    }
-                                    {
-                                        percentageOfTotalOthers > 0 &&
-                                        <View style={[styles.bar, {backgroundColor: COLORS.blue},{flex: percentageOfTotalOthers}]}>
-                                        </View>
-                                    }
+                            </View>
+                        </View>
 
-                                    {/*<Text style={{color: 'white'}}>{percentageOfTotalEmoji}</Text>*/}
-                                </View>
-                            )
-                        })
-                    }
-                </View>
-                </View>
-                {/*<View style={{width: '100%', height: 75, position: 'absolute', justifyContent: 'flex-end',}}>*/}
-                {/*    <Text style={{color: 'white', opacity: 0.5, alignSelf: 'flex-end'}}>average</Text>*/}
-                {/*    <Text numberOfLines={1} ellipsizeMode="clip" style={{color: 'white', opacity: 0.5}}*/}
-                {/*    >_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ </Text>*/}
-
-                {/*</View>*/}
-            </ScrollView>
             <View style={{flexDirection: 'row', justifyContent: 'space-around', paddingHorizontal: 15, marginVertical: 15,}}>
                 <View style={{flexDirection: 'row', gap: 10, marginTop: 10}}>
                     <View style={[styles.square,{backgroundColor: COLORS.orange}]}></View>
-                    <Text style={{color: 'white'}}>Message</Text>
+                    <Text style={styles.squareText}>Message</Text>
                 </View>
                 <View style={{flexDirection: 'row', gap: 10, marginTop: 10}}>
                     <View style={[styles.square,{backgroundColor: COLORS.green}]}></View>
-                    <Text style={{color: 'white'}}>Media</Text>
+                    <Text style={styles.squareText}>Media</Text>
                 </View>
                 <View style={{flexDirection: 'row', gap: 10, marginTop: 10}}>
                     <View style={[styles.square,{backgroundColor: COLORS.pink}]}></View>
-                    <Text style={{color: 'white'}}>Emoji</Text>
+                    <Text style={styles.squareText}>Emoji</Text>
                 </View>
                 <View style={{flexDirection: 'row', gap: 10, marginTop: 10}}>
                     <View style={[styles.square,{backgroundColor: COLORS.blue}]}></View>
-                    <Text style={{color: 'white'}}>Others</Text>
+                    <Text style={styles.squareText}>Others</Text>
                 </View>
             </View>
 
@@ -136,7 +168,7 @@ const styles = StyleSheet.create({
         fontSize: 17
     },
     bar: {
-        width: 45,
+        width: '100%',
         height: 150,
         backgroundColor: COLORS.orange,
         borderRadius: 15,
@@ -156,9 +188,13 @@ const styles = StyleSheet.create({
         marginBottom: 25,
     },
     square: {
-        width: 20,
-        height: 20,
+        width: 15,
+        height: 15,
         borderRadius: 5,
 
+    },
+    squareText: {
+        color: 'white',
+        fontSize: 12,
     }
 });
